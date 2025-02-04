303bc718fe392a6016cf8dee0180978c78afdbcf
Mateus Assis <mateus.d.assis.silva@gmail.com>
2025-02-04 17:34:59 -0300

    test: Add method

--- wordcounter/wordcounter_test.go
+++ wordcounter/wordcounter_test.go
@@ -2,6 +2,11 @@ package wordcounter
 
 import "testing"
 
+func initializeEmptyCounter() *WordCounter{
+	return NewWordCounter()
+}
+
+
 func TestNewWordCounter(t *testing.T) {
 	wordCounter := NewWordCounter()
 
@@ -10,4 +15,22 @@ func TestNewWordCounter(t *testing.T) {
 	if qtdWords != 0 {
 		t.Fail()
 	}
+}
+
+func TestAdd(t *testing.T){
+	counter := initializeEmptyCounter()
+
+	counter.Add("livro")
+	counter.Add("livro")
+	counter.Add("cinema")
+
+	t.Log(counter.countMap)
+	if counter.countMap["livro"] != 2 {
+		t.Fail()
+	}
+
+	if counter.countMap["cinema"] != 1 {
+		t.Fail()
+	}
+	
 }
