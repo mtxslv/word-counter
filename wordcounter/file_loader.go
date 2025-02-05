d6e4a22c90aa4b7a8a27c32d9c805005cb5f786b
Mateus Assis <mateus.d.assis.silva@gmail.com>
2025-02-05 12:45:52 -0300

    feat: file loader (untested)

---
+++ wordcounter/file_loader.go
@@ -0,0 +1,24 @@
+package wordcounter
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func loadText(textPath string) string {
+	// adapted from https://stackoverflow.com/questions/36111777/how-to-read-a-text-file
+	// and from https://stackoverflow.com/questions/9644139/from-io-reader-to-string-in-go
+    file, err := os.Open(textPath)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer func() {
+        if err = file.Close(); err != nil {
+            log.Fatal(err)
+        }
+    }()
+  b, err := io.ReadAll(file)
+  text := string(b)
+  return text
+}
